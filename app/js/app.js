283826a5d50f98a12ccae15fbb5ef848684da8ac
rcoor <theis@athgene.com>
2016-01-17 10:50:24 +0100

    [TASK] build out post page

--- app/js/app.js
+++ app/js/app.js
@@ -8,10 +8,34 @@
 			'app.controller.addpost',
 			'app.controller.toolbar',
 		])
-		.config(['$locationProvider','$stateProvider','$urlRouterProvider', Config]);
+		.config(['$locationProvider','$stateProvider','$urlRouterProvider', Config])
+		.config(function($mdThemingProvider) {
+			$mdThemingProvider.definePalette('fb-ai-palette', {
+				'50': '#8fa4cf',
+				'100': '#7e96c7',
+				'200': '#6c87c0',
+				'300': '#5a79b8',
+				'400': '#4b6bae',
+				'500': '#43609c',
+				'600': '#3b558a',
+				'700': '#344a78',
+				'800': '#2c3f66',
+				'900': '#243455',
+				'A100': '#a1b3d7',
+				'A200': '#b3c1de',
+				'A400': '#c5d0e6',
+				'A700': '#1d2943',
+		    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+		    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+		    '200', '300', '400', 'A100'],
+		    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+		  });
+		  $mdThemingProvider.theme('default')
+		  .primaryPalette('fb-ai-palette')
+		});
 
 	function Config($locationProvider, $stateProvider, $urlRouterProvider) {
-		$locationProvider.html5Mode(false);
+		$locationProvider.html5Mode(true);
 
 		$stateProvider
 				.state('home', {
--- app/js/controllers/addpost.js
+++ app/js/controllers/addpost.js
@@ -3,31 +3,51 @@
 		.module('app.controller.addpost', [])
 		.controller('AddpostController', AddpostController);
 
-	// Dont use the injects this gets sorted out by ng-annotate
+	/**
+	 *	Dont use the injects this gets sorted out by ng-annotate
+	 */
 	function AddpostController($scope) {
 		var addpost = this;
 		date = new Date();
-		// addpost is the object available to the view
+		/**
+		 *	addpost is the object available to the view
+		 */
 		angular.extend(addpost, {
-			title: null,
-			contents: null,
-			test_data: [{title: "This is my first post", content: "Here you will se a resume of the content", date: date},
-			{title: "Second post about NYC marathon", content: "The runners perfomed quite well at the marathon. Some did the distance in less than 5 minutes... Truly amazing.", date: date},
+			title: '',
+			contents: 'adadad #heje #hdhad',
+			hashtags: '',
+			sendPost,
+			test_data: [{title: 'This is my first post', content: 'Here you will se a resume of the content', date: date},
+			{title: 'Second post about NYC marathon', content: 'The runners perfomed quite well at the marathon. Some did the distance in less than 5 minutes... Truly amazing.', date: date},
 		]
 		});
-		$scope.sendPost = function () {
+
+		/**
+		 *	Here we match hashtags by regex and store them
+		 */
+		addpost.hashtags = getHashtags();
+
+		function getHashtags() {
+			return addpost.contents.match(/#\S+/g);
+		}
+
+		function sendPost() {
 			if (addpost.title) {
 				date = new Date();
-				console.log("Hej")
-		    addpost.test_data.push({title: addpost.title, content: addpost.contents, date: date})
+				addpost.test_data.push({
+					title: addpost.title,
+					content: addpost.contents,
+					date:	date
+				});
+
 				// Some $http here
 				// if success do following:
-				addpost.title = null;
-				addpost.contents = null;
+				addpost.title = '';
+				addpost.contents = '';
+				addpost.hashtags = '';
 			} else {
-			// items is still null
+				// items is still null
 			}
-
 		};
 	}
 })();
