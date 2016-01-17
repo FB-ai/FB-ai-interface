25176d6a73eecf1b3a9c9c12c8268966978919d9
Simon Dragsbæk Petersen <dev@nomis.dk>
2016-01-17 13:44:43 +0100

    [TASK] fix up the manifest file

--- gulp/manifest.js
+++ gulp/manifest.js
@@ -1,17 +1,18 @@
 // Import modules
 var config = require('./_config');
-var manifest = require('gulp-manifest');
+var manifest = require('gulp-appcache');
 
 // Takana module
 module.exports = function(gulp){
 	gulp.task('manifest', function(){
-		return gulp.src([config.output])
+		return gulp.src(config.manifest.watch, { base: './dist/' })
 			.pipe(manifest({
 				hash: true,
 				preferOnline: true,
+				network: ['http://*', 'https://*', '*'],
 				filename: 'app.manifest',
-				exclude: 'app.manifest'
+				exclude: config.manifest.exclude
 			}))
-			.pipe(gulp.dest(config.output));
+			.pipe(gulp.dest(config.manifest.dist));
 	});
 };
