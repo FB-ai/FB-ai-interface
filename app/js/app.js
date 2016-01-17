8d95d76d952215183beba71cdcda0b1b25e23ac4
Simon Dragsbæk Petersen <dev@nomis.dk>
2016-01-17 13:54:01 +0100

    [BUGFIX] clean up app.js

--- app/js/app.js
+++ app/js/app.js
@@ -8,33 +8,9 @@
 			'app.controller.addpost',
 			'app.controller.toolbar',
 		])
-		.config(['$locationProvider','$stateProvider','$urlRouterProvider', Config])
-		.config(function($mdThemingProvider) {
-			$mdThemingProvider.definePalette('fb-ai-palette', {
-				'50': '#8fa4cf',
-				'100': '#7e96c7',
-				'200': '#6c87c0',
-				'300': '#5a79b8',
-				'400': '#4b6bae',
-				'500': '#43609c',
-				'600': '#3b558a',
-				'700': '#344a78',
-				'800': '#2c3f66',
-				'900': '#243455',
-				'A100': '#a1b3d7',
-				'A200': '#b3c1de',
-				'A400': '#c5d0e6',
-				'A700': '#1d2943',
-		    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-		    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-		    '200', '300', '400', 'A100'],
-		    'contrastLightColors': undefined    // could also specify this if default was 'dark'
-		  });
-		  $mdThemingProvider.theme('default')
-		  .primaryPalette('fb-ai-palette')
-		});
+		.config(Config);
 
-	function Config($locationProvider, $stateProvider, $urlRouterProvider) {
+	function Config($locationProvider, $stateProvider, $urlRouterProvider, $mdThemingProvider) {
 		$locationProvider.html5Mode(true);
 
 		$stateProvider
@@ -53,5 +29,35 @@
 
 		$urlRouterProvider
 				.otherwise('/');
+
+		$mdThemingProvider.definePalette('fb-ai-palette', {
+			'50': '#8fa4cf',
+			'100': '#7e96c7',
+			'200': '#6c87c0',
+			'300': '#5a79b8',
+			'400': '#4b6bae',
+			'500': '#43609c',
+			'600': '#3b558a',
+			'700': '#344a78',
+			'800': '#2c3f66',
+			'900': '#243455',
+			'A100': '#a1b3d7',
+			'A200': '#b3c1de',
+			'A400': '#c5d0e6',
+			'A700': '#1d2943',
+			'contrastDefaultColor': 'light',
+			'contrastDarkColors': [
+					'50',
+					'100',
+					'200',
+					'300',
+					'400',
+					'A100'
+			],
+			'contrastLightColors': undefined
+		});
+
+		$mdThemingProvider.theme('default')
+			.primaryPalette('fb-ai-palette')
 	}
 })();
